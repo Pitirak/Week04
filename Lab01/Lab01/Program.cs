53bad1eb19a7cbce4ee61ceb21022b2e3d3a4802
“USER <bom_rsl4@hotmail.com>
2017-09-01 11:31:50 +0700

    No.8.9

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -8,7 +8,7 @@ namespace Lab01
         static void Main(string[] args)
         {
             string name = "Hello";
-            Console.WriteLine("The value: {0:C}.", 500);
+            Console.WriteLine("{0,-10:F4}", 12.3456789);
         }
     }
 }
