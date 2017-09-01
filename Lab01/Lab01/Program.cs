33e7c189ef58fe2838242880cf7c231d6e34095c
“USER <bom_rsl4@hotmail.com>
2017-09-01 11:55:19 +0700

    No.8.14

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -9,8 +9,8 @@ namespace Lab01
         {
             int i;
             Console.WriteLine("Value\tSquared\tCubed");
-            for (i = 1; i < 10; i++) ;
-
+            for (i = 1; i < 10; i++)
+                Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
         }
     }
 }
