6d1b3d1aab5a07b5332f9b7ab56cfe2b31896bc3
“USER <bom_rsl4@hotmail.com>
2017-09-01 11:25:49 +0700

    No.8.5

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -8,7 +8,7 @@ namespace Lab01
         static void Main(string[] args)
         {
             string name = "Hello";
-            Console.WriteLine("Here comes a slash \\");
+            Console.WriteLine("|{0, 10}|", 999);
         }
     }
 }
