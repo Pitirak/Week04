2aa7a335eb1ed0161ff4d32b6d0c1c402c8ef86a
“USER <bom_rsl4@hotmail.com>
2017-09-01 11:22:42 +0700

    No.8.3

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -8,7 +8,7 @@ namespace Lab01
         static void Main(string[] args)
         {
             string name = "Hello";
-            Console.WriteLine("{2:d} {0:d} {1:d}", 1, 2, 3);
+            Console.WriteLine("Hello " + "World");
         }
     }
 }
